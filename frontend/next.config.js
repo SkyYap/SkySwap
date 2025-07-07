d2b41a4dc419ee08257589f77370e57f81df2cdb
SkyYap <32660903+SkyYap@users.noreply.github.com>
2025-07-07 20:53:25 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
-          },
-        ],
-      },
-    ];
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /HeartbeatWorker\.js$/,
